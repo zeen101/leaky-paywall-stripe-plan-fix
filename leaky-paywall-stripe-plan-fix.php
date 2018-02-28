b37ac414f32780017fe06619dcbd3fd0c91bf714
Jeremy Green <greenhornet79@gmail.com>
2018-02-28 14:38:47 -0700

    initial commit

---
+++ leaky-paywall-stripe-plan-fix.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Leaky Paywall - Stripe Plan Fix
+ * Plugin URI: https://zeen101.com
+ * Description: This is a fix for Stripe accounts that are using an API version greater than 2018-02-01.
+ * Version: 1.0.0
+ * Author: Zeen101
+ * Author URI: https://zeen101.com
+ * Text Domain: mytextdomain
+ * License: GPL2
+ */
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Create a plan with the product field 
+// https://stripe.com/docs/api#create_plan
+add_filter( 'leaky_paywall_create_stripe_plan', 'lp_create_product_for_stripe_plan' );
+
+function lp_create_product_for_stripe_plan( $args, $level, $level_id ) {
+
+	$args['product'] = array(
+		'name' => esc_js( $level['label'] ) . ' ' . time(),
+		'type'	=> 'service'
+	);
+
+	unset( $args['name'] );
+
+	return $args;
+
+}
